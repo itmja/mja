211e98705db0911c53f1367b580ca22571f782e0
itmja <108081128+itmja@users.noreply.github.com>
2022-07-08 02:27:56 +0700

    Update employees.php

--- application/views/admin/reports/employees.php
+++ application/views/admin/reports/employees.php
@@ -12,7 +12,7 @@
         <input type="hidden" id="user_id" value="0" />
         <?php $attributes = array('name' => 'employee_reports', 'id' => 'employee_reports', 'autocomplete' => 'off', 'class' => 'add form-hrm');?>
 		<?php $hidden = array('euser_id' => $session['user_id']);?>
-        <?php echo form_open('admin/reports/employee_reports', $attributes, $hidden);?>
+        <?php echo form_open('admin/reports/employees');?>
         <?php
                 $data = array(
                   'name'        => 'user_id',
@@ -24,15 +24,6 @@
             echo form_input($data);
             ?> 
           <div class="form-row">
-          <?php if($user_info[0]->user_role_id==1){ ?>
-            <div class="col-md mb-3">
-              <label class="form-label">Status</label>
-              <select class="form-control"  name="status" id="">
-                <option value="">Semua</option>
-                <option value="1">Aktif</option>
-                <option value="12">Tidak Aktif</option>
-              </select>
-            </div>
             <div class="col-md mb-3">
               <label class="form-label"><?php echo $this->lang->line('left_company');?></label>
               <select class="form-control" name="company_id" id="aj_company" data-plugin="select_hrm" data-placeholder="<?php echo $this->lang->line('left_company');?>">
@@ -42,40 +33,29 @@
                 <?php } ?>
               </select>
             </div>
-            <?php } else {?>
-            <?php $ecompany_id = $user_info[0]->company_id;?>
-            <div class="col-md mb-3">
-              <label class="form-label"><?php echo $this->lang->line('left_company');?></label>
-              <select class="form-control" name="company_id" id="aj_company" data-plugin="select_hrm" data-placeholder="<?php echo $this->lang->line('left_company');?>">
-                <option value=""><?php echo $this->lang->line('left_company');?></option>
-                <?php foreach($all_companies as $company) {?>
-                <?php if($ecompany_id == $company->company_id):?>
-                <option value="<?php echo $company->company_id?>"><?php echo $company->name?></option>
-                <?php endif;?>
-                <?php } ?>
+            <div class="col-md mb-3" id="department_ajax">
+              <label class="form-label"><?php echo $this->lang->line('left_department');?></label>
+              <select class="form-control" id="filter_department" name="department_id" data-plugin="select_hrm" data-placeholder="<?php echo $this->lang->line('left_department');?>" >
+                <option value="0"><?php echo $this->lang->line('xin_acc_all');?></option>
               </select>
             </div>
-            <?php } ?>
-            <div class="col-md mb-3">
-            <div class="form-group" id="department_ajax">
-              <label class="form-label"><?php echo $this->lang->line('xin_employee_department');?></label>
-              <select disabled="disabled" class="form-control" name="department_id" id="department_id" data-plugin="select_hrm" data-placeholder="<?php echo $this->lang->line('xin_employee_department');?>">
-                    <option value="0"><?php echo $this->lang->line('xin_acc_all');?></option>
-                  </select>
-               </div>   
+            <div class="col-md mb-3" id="designation_ajax">
+              <label class="form-label"><?php echo $this->lang->line('xin_designation');?></label>
+              <select class="form-control" name="designation_id" data-plugin="select_hrm"  id="designation" data-placeholder="<?php echo $this->lang->line('xin_designation');?>">
+                <option value="0"><?php echo $this->lang->line('xin_acc_all');?></option>
+              </select>
             </div>
-            <div class="col-md-3" id="designation_ajax">
-                <div class="form-group">
-                  <label for="designation"><?php echo $this->lang->line('xin_designation');?></label>
-                  <select disabled="disabled" class="form-control" id="designation_id" name="designation_id" data-plugin="select_hrm" data-placeholder="<?php echo $this->lang->line('xin_designation');?>">
-                    <option value="0"><?php echo $this->lang->line('xin_acc_all');?></option>
-                  </select>
-                </div>
-              </div>         
+            <div class="col-md mb-3" >
+              <label class="form-label">Status Karyawan</label>
+              <select name="active" id="active" class="form-control" data-plugin="" data-placeholder="Status Karyawan">
+                <option value="">Semua</option>
+                <option value="0">Tidak Aktif</option>
+                <option value="1">Aktif</option>
+              </select>
+            </div>
             <div class="col-md col-xl-2 mb-4">
               <label class="form-label d-none d-md-block">&nbsp;</label>
-              <button type="submit" class="btn btn-secondary btn-block"><?php echo $this->lang->line('xin_get');?></button>
-            </div>
+              <?php echo form_button(array('name' => 'hrsale_form', 'type' => 'submit', 'class' => 'btn btn-secondary btn-block', 'content' => '<i class="fas fa-check-square"></i> '.$this->lang->line('xin_get'))); ?> </div>
           </div>
           <?php echo form_close(); ?>
         </div>
@@ -87,7 +67,7 @@
       <div class="card-header with-elements"> <span class="card-header-title mr-2"><strong><?php echo $this->lang->line('xin_hr_report_employees');?></strong></span> </div>
       <div class="card-body">
         <div class="box-datatable table-responsive">
-          <table class="datatables-demo table table-striped table-bordered" id="xin_table">
+          <table class="datatables-demo table table-striped table-bordered" id="example">
             <thead>
               <tr>
                 <th><?php echo $this->lang->line('xin_employees_id');?></th>
@@ -99,6 +79,27 @@
                 <th><?php echo $this->lang->line('dashboard_xin_status');?></th>
               </tr>
             </thead>
+            <tbody>
+              <?php
+                foreach($tampil as $ls){
+              ?>
+              <tr>
+                <td><?= $ls->employee_id ?></td>
+                <td><?= $ls->first_name ?> <?= $ls->last_name ?></td>
+                <td><?= $ls->name ?></td>
+                <td><?= $ls->email1 ?></td>
+                <td><?= $ls->department_name ?></td>
+                <td><?= $ls->designation_name ?></td>
+                <td><?php
+                  if($ls->active === "0"){
+                    echo "Tidak Aktif";
+                  }else{
+                    echo "Aktif";
+                  }
+                ?></td>
+              </tr>
+              <?php } ?>
+            </tbody>
           </table>
         </div>
       </div>
@@ -106,3 +107,13 @@
   </div>
 </div>
 
+<script>
+  $(document).ready(function() {
+    $('#example').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+} );
+</script>
